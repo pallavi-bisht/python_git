f91c853bc7fd58b6747311ed3142ce84950cf2de
pallavi-bisht <pallavi111193@gmail.com>
2024-05-20 23:40:42 +0530

    Commit

--- Tuple.py
+++ Tuple.py
@@ -1,3 +1,5 @@
+#defining Tuple 
+# Tuple are list which are immutable
 Menu=('Roti','Sabji','Rice','dal','sweet')
 for i in Menu:
  print(i)
@@ -5,3 +7,15 @@ for i in Menu:
 # can't change value of tuple
 Menu=('Roti','Sabji','Rice','dal','sweet')
 Menu[4]='cream'
+
+# Reassign tuple compltetely
+Menu=('Roti','Sabji','Rice','dal','sweet')
+print(Menu)
+
+Menu=('Roti','Sabji',1)
+for i in Menu:
+ print(i)
+
+
+
+
