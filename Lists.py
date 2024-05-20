059f48e4403c3d605fa1dbafb770e528dea5e91c
pallavi-bisht <pallavi111193@gmail.com>
2024-05-20 19:50:52 +0530

    list 2

--- Lists.py
+++ Lists.py
@@ -1,2 +1,34 @@
-abc=[1,2,3,4]
+abc=[1,2,3,"sing"]
 print(abc)
+print(abc[1])
+print(abc[-1])
+
+#add
+abc.append('pallavi')
+print(abc)
+
+#delete  by position
+abc=[1,2,3,"sing"]
+pop_abc=abc.pop(0)
+print(f"{abc} {pop_abc}")
+
+#delete by value
+abc=[1,2,3,"sing"]
+abc.remove("sing")
+print(abc)
+
+#sort method ,sorted function,reverse method
+
+abc=[2,4,1,7]
+abc.sort()
+print(abc)
+klm=[8,33,45,2]
+print(sorted(klm))
+abc=[2,4,1,7]
+abc.reverse()
+print(abc)
+
+
+
+
+
