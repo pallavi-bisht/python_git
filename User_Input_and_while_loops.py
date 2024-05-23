c01cb8b7b7604747b2b0d71f0e20d59672c2fc9c
pallavi-bisht <pallavi111193@gmail.com>
2024-05-23 17:12:11 +0530

    commit

--- User_Input_and_while_loops.py
+++ User_Input_and_while_loops.py
@@ -1,2 +1,29 @@
 message=input("Tell me something i will repeat after you.")
 print(message)
+
+Prompt="Tell me something i will repeat after you."
+Prompt+="\nsomething interesting"
+name =input(Prompt)
+print(name)
+
+#TIY 117
+
+car= input('Which is your fav car\n')
+print(f"\nlet me find you a {car}")
+
+table=input("How many people are in your group?\t")
+ 
+if int(table)>8:
+    print("\nSorry you will have to wait!")
+else:
+    print("\nyour table is ready!!")     
+
+number= input("Enter a number \t")
+
+if int(number)%10==0:
+    print("\nIts is a multiple of 10 .")
+else:
+    print("\nNot a multiple of 10")    
+
+#TIY 123
+ 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+number= input("Enter a number \t")
+
+if int(number)%10==0:
+    print("\nIts is a multiple of 10 .")
+else:
+    print("\nNot a multiple of 10")  
