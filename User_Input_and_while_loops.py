f548dfc4ef16bc95e7757db5e9a3333ce979145c
pallavi-bisht <pallavi111193@gmail.com>
2024-05-23 18:11:36 +0530

    commit

--- User_Input_and_while_loops.py
+++ User_Input_and_while_loops.py
@@ -74,6 +74,24 @@ while 'pastrami' in sandwitch_orders:
 print(sandwitch_orders)
 
 
+response={}
+prompt1="What is you name.\n"
+prompt2="what is favourite destination?\n "
+
+while True:
+    name=input(prompt1)
+    place=input(prompt2)
+    response[name]=place
+    ans=input("Do you want to quit! (y/n)")
+    if ans=='y':
+      break
+      
+print(response)       
+
+
+
+
+
 
 
    
