b371d24a313010e0ba5b54c816f8ac0167f9936b
pallavi-bisht <pallavi111193@gmail.com>
2024-05-24 16:58:01 +0530

    commit

---
+++ Function.py
@@ -0,0 +1,19 @@
+#TIY 131
+
+def display_message():
+    print("I am reading function chapter in python.")
+
+display_message()
+
+def favorite_book(title):
+    print(f"MY favorite book is {title}")
+
+favorite_book("Maths")    
+
+#TIY 136
+
+def make_shirt(size,msg):
+    print(f"The size of short is {size} and message is {msg}")
+
+make_shirt(34,'I am a liver')    
+make_shirt(size=32,msg='I am a pancreas')
