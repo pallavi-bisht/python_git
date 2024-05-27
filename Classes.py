afb7b72ff0fbe8d96df05164d54b7f2c09575eb8
pallavi-bisht <pallavi111193@gmail.com>
2024-05-27 19:55:54 +0530

    commit

--- Classes.py
+++ Classes.py
@@ -38,6 +38,136 @@ pqr=User('pallavi','Bisht','30')
 pqr.describe_user()
 pqr.greet_user()
 
+#TIY 166
+
+class Restaurant:
+    
+    def __init__(self,rest_name,rest_cuisine):
+        self.rest_name=rest_name
+        self.rest_cuisine=rest_cuisine
+        self.number_served=0
+
+    def describe_rest(self):
+        print(f"Restaurant name: {self.rest_name}\n")
+        print(f"Cusisine name: {self.rest_cuisine}\n")
+        print(f"Number of cusine served {self.number_served}.\n")
+    
+    def set_number_served(self,number_served):
+        self.number_served=number_served
+    
+    def increment_number_served(self,number_served):
+        self.number_served+=number_served
+
+
+abc= Restaurant("SALT",'North Indian') 
+abc.describe_rest()
+abc.number_served=20
+abc.describe_rest()
+abc.set_number_served(18)
+abc.describe_rest()
+abc.increment_number_served(30)
+abc.describe_rest()
+
+
+class User:
+
+    def __init__(self,f_name,l_name):
+        self.f_name=f_name
+        self.l_name=l_name
+        self.loggin_attempt=0
+    
+    def  describe_user(self):
+
+        print(f"User's Name is {self.f_name} {self.l_name} and attemt is  {self.loggin_attempt}")
+    
+    def increment_attempt(self): 
+        self.loggin_attempt=self.loggin_attempt+1
+
+    def  reset_attempt(self):
+        self.loggin_attempt=0    
+
+pqr=User('pallavi','Bisht')
+pqr.describe_user()
+pqr.increment_attempt()
+pqr.describe_user()
+pqr.increment_attempt()
+pqr.describe_user()
+pqr.increment_attempt()
+pqr.describe_user()
+pqr.reset_attempt()
+pqr.describe_user()
+
+#TIY 173
+
+class Restaurant:
+    
+    def __init__(self,rest_name,rest_cuisine):
+        self.rest_name=rest_name
+        self.rest_cuisine=rest_cuisine
+
+    def describe_rest(self):
+        print(f"Restaurant name: {self.rest_name}\n")
+        print(f"Cusisine name: {self.rest_cuisine}\n")
+    
+    def open_rest(self):
+        print(f"Restaurant {self.rest_name} is open!\n")
+
+
+class IceCreamStand(Restaurant):
+
+    def __init__(self,rest_name,rest_cuisine):
+        super().__init__(rest_name,rest_cuisine)
+        self.flavour=['vanilla','chocolate','pista']
+    def display_flavor(self):
+        for i in self.flavour:
+            print(f"{i}\n")
+
+lmn=IceCreamStand('Piggy','Ice Cream')
+lmn.display_flavor()
+
+# Instance as attribute 
+
+class User:
+
+    def __init__(self,f_name,l_name,age):
+        self.f_name=f_name
+        self.l_name=l_name
+        self.age=age
+    
+    def  describe_user(self):
+
+        print(f"User's Name is {self.f_name} {self.l_name} and age is {self.age}")
+
+    def greet_user(self):
+
+        print(f"Welcome {self.f_name} {self.l_name} !") 
+
+class admin(User):
+    
+    def __init__(self,f_name,l_name,age):
+        super().__init__(f_name,l_name,age)
+        self.priv=privileges()
+
+class privileges:
+
+    def __init__(self):
+        self.privileges=["can reset post",'can delete post','can ban user']
+    def show_prvileges(self):
+        print("privileges are :\n")
+        for i in self.privileges:
+            print(f" {i}\n")    
+
+fgh=admin('Arvind','bisht',20)    
+fgh.priv.show_prvileges() 
+
+
+
+
+
+
+        
+
+
 
 
 
